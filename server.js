4a48f46d5acdf02cd78270a46f6021c8bf242eb3
Josh Hiles <josh.hiles@aroflo.com>
2021-06-06 11:23:29 +1000

    added a few routes and default json file.

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 const express = require("express");
+const connectDB = require("./config/db");
 
 const app = express();
 
+// Connect database
+connectDB();
+
 app.get("/", (req, res) => res.send("API running."));
+
+// Define routes for endpoints - where these defined endpoints will point to in my file structure.
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/posts", require("./routes/api/posts"));
 
 // Will watch port set in the env variable once deployed on Heroku. otherwise it'll watch port 5000.
 const PORT = process.env.PORT || 5000;
